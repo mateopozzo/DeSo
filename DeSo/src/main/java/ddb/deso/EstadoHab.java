1bf8c42492577a9325c7c5eed4e3c7e2e5a9784a
GIEDI - UTN FRSF <giedifrsfutn@gmail.com>
2025-10-07 19:07:21 -0300

    Paquete habitaciones: Clases y ennumeraciones

---
+++ DeSo/src/main/java/ddb/deso/EstadoHab.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ddb.deso;
+
+/**
+ *
+ * @author gael-deb
+ */
+public enum EstadoHab {
+    DISPONIBLE, OCUPADA, RESERVADA, ENMANTENIMIENTO;
+}
