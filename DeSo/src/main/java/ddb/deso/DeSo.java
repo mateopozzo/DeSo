32faddca0dfd8392691954c8aef75c2bf4c56a97
Mateo <mateopozzogaldon@gmail.com>
2025-10-07 18:05:18 -0300

    Crear proyecto

---
+++ DeSo/src/main/java/ddb/deso/DeSo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ddb.deso;
+
+/**
+ *
+ * @author mat
+ */
+public class DeSo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
