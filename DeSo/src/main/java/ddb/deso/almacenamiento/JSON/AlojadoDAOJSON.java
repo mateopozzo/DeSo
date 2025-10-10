490f6ba6128086f9004635c9f1c4d0027c309778
Mateo <mateopozzogaldon@gmail.com>
2025-10-10 16:15:38 -0300

    agrego dependencia gson

---
+++ DeSo/src/main/java/ddb/deso/almacenamiento/JSON/AlojadoDAOJSON.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ddb.deso.almacenamiento.JSON;
+
+import ddb.deso.TipoDoc;
+import ddb.deso.almacenamiento.DAO.AlojadoDAO;
+import ddb.deso.alojamiento.Alojado;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mat
+ */
+public class AlojadoDAOJSON implements AlojadoDAO {
+    void crearAlojado(Alojado alojado){
+        
+    }
+    void actualizarAlojado(Alojado alojado){
+    }
+    void eliminarAlojado(Alojado alojado){
+    }
+    ArrayList<Alojado> listarAlojados(){
+    }
+    Alojado buscarPorDNI(String documento, TipoDoc tipo){
+    }
+}
