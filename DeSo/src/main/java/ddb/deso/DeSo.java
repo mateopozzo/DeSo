718ac33bdb7c9e65e87f057817e8d036f9faae17
Gael <gaelbelletti@gmail.com>
2025-10-22 13:08:47 -0300

    Problemitas varios de sincronización

--- DeSo/src/main/java/ddb/deso/DeSo.java
+++ DeSo/src/main/java/ddb/deso/DeSo.java
@@ -8,15 +8,9 @@ import ddb.deso.alojamiento.GestorAlojamiento;
  */
 public class DeSo {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello World!");
 GestorAlojamiento ga=new GestorAlojamiento();
-
 ga.darDeAltaHuesped();
 
-
-=======
-        System.out.println("Hello World!");       
->>>>>>> 02c0b6a5890096dd1a6ad1cdd911de5593caf083
     }
 }
