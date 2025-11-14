bc25c88d2c323265b81eb2bd72d171cbbc6442fa
Mateo <mateopozzogaldon@gmail.com>
2025-11-14 19:07:50 -0300

    persistencia

---
+++ DeSo/src/main/java/ddb/deso/repository/AlojadoRepository.java
@@ -0,0 +1,11 @@
+package ddb.deso.repository;
+
+import ddb.deso.alojamiento.Alojado;
+import ddb.deso.alojamiento.AlojadoID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlojadoRepository extends JpaRepository<Alojado, AlojadoID> {
+
+}
