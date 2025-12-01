c3e1227bd5ef2934782592947287c96e9043bafa
Mateo <mateopozzogaldon@gmail.com>
2025-12-01 11:20:07 -0300

    Agrega resources/static con front compilado

--- DeSo/hotel-premier/next.config.ts
+++ DeSo/hotel-premier/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  //output: 'export', -- Para compilar static
+  //reactCompiler: true,
+  output: 'export', //-- Para compilar static
 };
 
-//module.exports = nextConfig;
- export default nextConfig;
+module.exports = nextConfig;
+export default nextConfig;
