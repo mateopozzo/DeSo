a90a239e993ab5b80375727a91c566ada9599472
Gael <gaelbelletti@gmail.com>
2025-12-07 18:15:37 -0300

    fix: Cuestiones estéticas para que ocupar y reservar sean parecidas

--- DeSo/hotel-premier/src/app/reservar-hab/page.tsx
+++ DeSo/hotel-premier/src/app/reservar-hab/page.tsx
@@ -140,6 +140,12 @@ export default function ReservarHab() {
     setPaso("GRILLA");
   };
 
+  const cancelarCasoUso = () => {
+    if (confirm("¿Desea cancelar todo el proceso?")) {
+      router.push("/");
+    }
+  };
+
   return (
     <div className="p-6 min-h-screen bg-[#f5f7fa] dark:bg-gray-950 dark:text-white">
       <h1 className=" dark:text-white text-5xl font-bold pb-2">
@@ -149,43 +155,53 @@ export default function ReservarHab() {
         Indique las fechas deseadas para verificar disponibilidad de
         habitaciones
       </p>
+      <hr className="mb-8 dark:text-white/50 text-black/50" />
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+          {paso === "GRILLA" && "Verificar disponibilidad de habitaciones"}
+          {paso === "FORMULARIO" && "Ingresar los datos del huésped"}
+          {paso === "GUARDANDO" && "Guardando..."}
+          {paso === "EXITO" && "Reserva exitosa"}
+        </h1>
+        <button
+          onClick={cancelarCasoUso}
+          className="cursor-pointer px-8 py-2 rounded-xl font-bold transition duration-300 dark:border dark:border-white dark:text-white bg-[#f5f7fa] dark:bg-gray-950 dark:hover:border-[#b92716] text-[#1a252f] border border-[#1a252f] hover:bg-[#b92716] hover:text-white hover:border-[#b92716]"
+        >
+          Cancelar
+        </button>
+      </div>
 
       {error && (
-        <div className="bg-red-500 text-white p-4 rounded-xl mb-6 font-bold shadow-md">
-          Error: {error}
+        <div className="bg-red-600 text-white p-4 rounded-xl mb-6 font-bold shadow-md">
+          {error}
         </div>
       )}
-
       {paso === "GRILLA" && (
         <div className="space-y-6 animate-fade-in">
           <div className="flex flex-wrap gap-4 items-end bg-[#f5f7fa] dark:bg-gray-950 p-6 rounded-2xl shadow-sm ">
             <div>
-              <label className="block text-sm font-bold mb-2 text-gray-700 dark:text-gray-300">
-                Desde
-              </label>
+              <label className="block text-md font-bold mb-2">Desde</label>
               <input
                 type="date"
                 value={fecha_inicio_busq}
                 onChange={(e) => setDesdeBusq(e.target.value)}
-                className="px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-700 bg-[#f5f7fa] dark:bg-gray-950 focus:ring-2 focus:ring-green-500 outline-none transition"
+                className="px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-700 bg-[#f5f7fa] dark:bg-gray-950 focus:ring focus:ring-blue-400 outline-none transition"
               />
             </div>
             <div>
-              <label className="block text-sm font-bold mb-2 text-gray-700 dark:text-gray-300">
-                Hasta
-              </label>
+              <label className="block text-md font-bold mb-2">Hasta</label>
               <input
                 type="date"
                 value={fecha_fin_busq}
                 onChange={(e) => setHastaBusq(e.target.value)}
-                className="px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-700 bg-[#f5f7fa] dark:bg-gray-950 focus:ring-2 focus:ring-green-500 outline-none transition"
+                className="px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-700 bg-[#f5f7fa] dark:bg-gray-950 focus:ring focus:ring-blue-400 outline-none transition"
               />
             </div>
             <button
               onClick={buscarDisponibilidad}
               className="px-8 py-2 rounded-xl font-bold bg-[#52a173] text-white hover:bg-[#10b655] shadow-lg hover:shadow-xl transition transform active:scale-95"
             >
-              Buscar
+              Ver disponibilidad
             </button>
           </div>
 
